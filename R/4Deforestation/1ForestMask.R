56171feb08287e655cbdc1688990518de642492b
johnsonojeda <johnson.ojeda@gmail.com>
2026-07-24 14:40:29 -0400

    Updated deforestation mask script . Terra package

--- R/4Deforestation/1ForestMask.R
+++ R/4Deforestation/1ForestMask.R
@@ -3,50 +3,47 @@
 ##########################################################
 ##' Removing areas falling below % tree cover threshold from vegetation ENMs
 
-library(raster)
-library(rgdal)
-library(rgeos)
+library(terra)
 
-##################################
-## 1. UPLOADING ENMs and VCF DATA
-##################################
+########################################################
+## 1. UPLOADING ENMs, OTHER LAND COVER, & VCF DATA
+########################################################
 # Loading unprocessed and post-processed ENMs
 
 #Continuous ENMs
 CH.enm.dir <- '/home/erica/PhD/Mex_VegModel/Analyses/ENM/Outputs/RasPreds/CarsoHuasteco'
 CH.enm.path <- file.path(CH.enm.dir, dir(CH.enm.dir, pattern = "25_mtp.tif$"))
-CH.enm <- raster::stack(CH.enm.path)
+CH.enm <- terra::rast(CH.enm.path)
 
 #WBC masked ENMs
 CH.wbc.dir <- '/home/erica/PhD/Mex_VegModel/Analyses/SVM/outputs/CarsoHuasteco/WBC'
 CH.wbc.path <- file.path(CH.wbc.dir, dir(CH.wbc.dir, pattern = "WBC_mtp.tif$"))
-CH.wbc <- raster::stack(CH.wbc.path)
+CH.wbc <- terra::rast(CH.wbc.path)
 
 #SVMsp masked ENMs
 CH.SVMsp.dir <- '/home/erica/PhD/Mex_VegModel/Analyses/SVM/outputs/CarsoHuasteco/SVMsp'
 CH.SVMsp.path <- file.path(CH.SVMsp.dir, dir(CH.SVMsp.dir, pattern = "SVMsp_mtp.tif$"))
-CH.SVMsp <- raster::stack(CH.SVMsp.path)
+CH.SVMsp <- terra::rast(CH.SVMsp.path)
 
 #SVMhyb masked ENMs
 CH.SVMhyb.dir <- '/home/erica/PhD/Mex_VegModel/Analyses/SVM/outputs/CarsoHuasteco/SVMhyb'
 CH.SVMhyb.path <- file.path(CH.SVMhyb.dir, dir(CH.SVMhyb.dir, pattern = "SVMhyb_mtp.tif$"))
-CH.SVMhyb <- raster::stack(CH.SVMhyb.path)
+CH.SVMhyb <- terra::rast(CH.SVMhyb.path)
 
 #Importing the 3-year MODIS VCF average raster for the Carso Huasteco
-VCF <- raster("data/MODIS_VCF/MODISVCF_2016-2018Avg.tif")
+VCF <- terra::rast("/home/erica/PhD/Mex_VegModel/Analyses/ForestMask/data/MODIS_VCF/MODISVCF_2016-2018Avg.tif")
 
 #Loading the Carso Huasteco shapefile matching it to the the VCF raster's spatial reference system
-CH <- rgdal::readOGR("/home/erica/PhD/Mex_VegModel/Analyses/ENM/data/CarsoHuasteco.shp", 
-                     layer = "CarsoHuasteco")
+CH <- terra::vect("/home/erica/PhD/Mex_VegModel/Analyses/ENM/data/CarsoHuasteco.shp")
 
-CHt <- spTransform(CH, CRS(projection(VCF)))
+CHt <- terra::project(CH, crs(VCF))
 
 #Cropping the VCF layer to the extent of the Carso Huasteco
-VCF <- crop(VCF, CHt)
-VCF<- mask(VCF, CHt)
+VCF <- terra::crop(VCF, CHt)
+VCF<- terra::mask(VCF, CHt)
 
 ############################################
-# 2. PERCENT TREE COVER ENM THRESHOLDING 
+# 3. PERCENT TREE COVER ENM THRESHOLDING 
 ############################################
 
 #' @param enm a raster object of enm predictions
@@ -54,62 +51,64 @@ VCF<- mask(VCF, CHt)
 
 #In WGS 84 (no projection) for visualization
 FC.thresh <- function(enm, mask, out.dir){
-  CF <- subset(enm, which(grepl("CF_", names(enm)) == TRUE))
-  PO <- subset(enm, which(grepl("PO_", names(enm)) == TRUE))
+  CF <- enm[[grep("^CF_", names(enm))]]
+  PO <- enm[[grep("^PO_", names(enm))]]
   
-  #subsetting the mask by forest cover threshold
+  #Cloud forest threshold(>=60% tree cover)
   CFm <- mask >= 60
   CFm[CFm == 0]<- NA
   
   # Reprojecting to match extent & resolution of ENMs
-  CFm <- projectRaster(CFm, CF)
-  CFm <- crop(CFm, CF)
+  CFm <- terra::project(CFm, CF, method = "near")
+  CFm <- terra::crop(CFm, CF)
   
+  #Pine-oak forest threshold (=>40% Forest cover <=60%)
   POm <- mask >= 40
   POm[POm == 0]<- NA
-  POm <- projectRaster(POm, PO)
-  POm <- crop(POm, PO)
+  POm <- terra::project(POm, PO, method = "near")
+  POm <- terra::crop(POm, PO)
   
   # Masking the vegetation ENMs
-  CFmsk <- mask(CF, CFm)
-  POmsk <- mask(PO, POm)
+  CFmsk <- terra::mask(CF, CFm)
+  POmsk <- terra::mask(PO, POm)
+  
+  #Export maksed ENM raster files
+  terra::writeRaster(CFmsk, file.path(out.dir, paste0(names(CFmsk), "_FC.tif")))
+
+  terra::writeRaster(POmsk, file.path(out.dir, paste0(names(POmsk), "_FC.tif")))
   
   #Creating a single raster stack of masked ENMs
-  enm.msk <- raster::stack(CFmsk, POmsk)
-  
-  #Exporting masked ENM raster files 
-  writeRaster(enm.msk, paste(out.dir, paste0(names(enm), "_FC"),
-                             sep = "/"), bylayer = T, format = "GTiff", overwrite = T) 
+  enm.msk <- c(CFmsk, POmsk)
   return(enm.msk)
 }
 
 #In sinusoidal for areal calculations
 FC.thresh2 <- function(enm, mask, out.dir){
-  CF <- subset(enm, which(grepl("CF_", names(enm)) == TRUE))
-  PO <- subset(enm, which(grepl("PO_", names(enm)) == TRUE))
+  CF <- enm[[grep("^CF_", names(enm))]]
+  PO <- enm[[grep("^PO_", names(enm))]]
   
   #Subsetting the mask by percent tree cover threshold
   CFm <- mask >= 60
   CFm[CFm == 0]<- NA
   
   # Reprojecting to match extent & resolution of ENM
-  CF <- projectRaster(CF, CFm)
+  CF <- terra::project(CF, CFm)
   
   POm <- mask >= 40 
   POm[POm == 0]<- NA
-  PO <- projectRaster(PO, POm)
+  PO <- terra::project(PO, POm)
   
   #Masking the vegetation ENMs
-  CFmsk <- mask(CF, CFm)
-  POmsk <- mask(PO, POm)
+  CFmsk <- terra::mask(CF, CFm)
+  POmsk <- terra::mask(PO, POm)
+  
+  #Export maksed ENM raster files
+  terra::writeRaster(CFmsk, file.path(out.dir, paste0(names(CFmsk), "_FC.tif")))
+
+  terra::writeRaster(POmsk, file.path(out.dir, paste0(names(POmsk), "_FC.tif")))
   
   #Creating a single raster stack of masked ENMs
-  enm.msk <- raster::stack(CFmsk, POmsk)
-  
-  #Exporting masked ENM raster files   
-  writeRaster(enm.msk, paste(out.dir, 
-                             paste0(names(enm), "_FC"), sep = "/"), 
-              bylayer = T, format = "GTiff", overwrite = T) 
+  enm.msk <- c(CFmsk, POmsk)
   
   return(enm.msk)
 }
@@ -122,7 +121,8 @@ FC.thresh2 <- function(enm, mask, out.dir){
 ENM.list <- list(CH.enm, CH.wbc, CH.SVMsp, CH.SVMhyb)
 
 #applying the forest cover threshold to all pre and post-processed predictions
-ENM.th <- lapply(ENM.list, FC.thresh, mask = VCF, out.dir = "outputs/WGS84")
+ENM.th <- lapply(ENM.list, FC.thresh, mask = VCF, out.dir = "/home/erica/PhD/Mex_VegModel/Analyses/ForestMask/outputs/WGS84")
+
 
 #applying the forest cover threshold to all predictions (in sinusoidal proj)
-ENM.th2 <- lapply(ENM.list, FC.thresh2, mask = VCF, out.dir = "outputs/Sinusoidal")
+ENM.th2 <- lapply(ENM.list, FC.thresh2, mask = VCF, out.dir = "/home/erica/PhD/Mex_VegModel/Analyses/ForestMask/outputs/Sinusoidal")
